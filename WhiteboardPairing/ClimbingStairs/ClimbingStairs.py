c2f32645dbd6d6db264f112ae2c50ae43c8bd51a
benjchand <benchand@gmail.com>
2019-08-21 23:37:10 -0700

    Working on ClimbingStairs.

---
+++ WhiteboardPairing/ClimbingStairs/ClimbingStairs.py
@@ -0,0 +1,3 @@
+# This is almost Fibonacci's sequence.  Rather than adding the two before, I believe you add the three before.
+# Running it against a dictionary and seeing if it exists might be the best way.
+# Perhaps check the other code that I believe is in the recursion section of the course.
